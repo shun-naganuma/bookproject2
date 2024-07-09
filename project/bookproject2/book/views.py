8d4beefbbb30ab2b59db5934b6dd7510aff0a425
shun-naganuma <129668177+shun-naganuma@users.noreply.github.com>
2024-07-09 18:30:51 +0900

    ４章完了

--- project/bookproject2/book/views.py
+++ project/bookproject2/book/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView,
+    UpdateView,
+    )
 from .models import Book
 
 class ListBookView(ListView):
@@ -7,5 +14,22 @@ class ListBookView(ListView):
     model = Book
 
 class DetailBookView(DetailView):
-    template_name = 'book.book_detail.html'
+    template_name = 'book/book_detail.html'
     model = Book
+
+class CreateBookView(CreateView):
+    template_name = 'book/book_create.html'
+    model = Book
+    fields = {'title', 'text', 'category'}
+    success_url = reverse_lazy('list-book')
+
+class DeleteBookView(DeleteView):
+    template_name = 'book/book_confirm_delete.html'
+    model = Book
+    success_url = reverse_lazy('list-book')
+
+class UpdateBookView(UpdateView):
+    model = Book
+    fields = {'title', 'text', 'category'}
+    template_name = 'book/book_update.html'
+    success_url = reverse_lazy('list-book')
